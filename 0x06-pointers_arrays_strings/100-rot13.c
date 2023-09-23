091a709ddafd862de678ebf1bb4fa7f57f43d080
areeg160 <aryjhafz20@gmail.com>
2023-09-22 17:53:56 -0700

    a

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,23 @@
-#include "holberton.h"
-/**
- * rot13 - change letters to ROT13.
- * @s: analized string.
- *
- * Return: String with all letters in ROT13 base.
- */
-char *rot13(char *s)
-{
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	int i = 0;
-	int j;
+#include <stdio.h>
 
-	while (*(s + i) != '\0')
-	{
-		for (j = 0; j <= 51; j++)
-		{
-			if (*(s + i) == a[j])
-			{
-				*(s + i) = rot[j];
-				break;
-			}
-		}
-		i++;
-	}
-	return (s);
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
